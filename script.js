b2f29084d3d7dbffeb7bace2c291d644eae02244
abhishekptiwari <abhishektiwariaman@gmail.com>
2021-09-20 14:58:02 +0530

    task Completed

---
+++ script.js
@@ -0,0 +1,82 @@
+var healthPoints = 100;
+
+function updateHealthPoints(points) {
+
+	healthPoints = points;
+	var healthBar = document.querySelector("#healthBar");
+	healthBar.style.width = points + "%";
+
+	if(healthPoints < 1) {
+		alert("Player 2 win!");//game over
+		//window.location.reload();
+	}
+
+}
+
+
+function livingEnemies() {
+	return document.querySelectorAll(".enemy:not(.dead)");
+}
+
+
+function iShoot(enemy) {
+	enemy.classList.add("dead");
+
+	if(!livingEnemies().length) {
+		alert("Player 1 win!");//you win
+		//window.location.reload();
+	}
+
+}
+
+
+function enemyAttacksMe(enemy) {
+
+	if(healthPoints > 0) {
+
+		enemy.classList.add("showing");
+
+		setTimeout(()=> {
+			enemyShootsMe(enemy);
+		}, 1000);
+
+		setTimeout(()=> {
+			enemy.classList.remove("showing");
+		}, 3000);
+		
+	}
+
+
+}
+
+
+function enemyShootsMe(enemy) {
+
+	if(!enemy.classList.contains("dead")) {
+
+		enemy.classList.add("shooting");
+		updateHealthPoints(healthPoints - 20);
+
+		setTimeout(()=> {
+			enemy.classList.remove("shooting");
+		}, 200);
+
+	}
+
+}
+
+
+function randomEnemyAttacks() {
+
+	var randomEnemyNo = Math.random() * livingEnemies().length;
+	randomEnemyNo = Math.floor(randomEnemyNo);
+	var enemy = livingEnemies()[randomEnemyNo];
+
+	var randomDelay = Math.random() * 2000 + 1000;
+
+	setTimeout( ()=> {
+		enemyAttacksMe(enemy);
+		randomEnemyAttacks();
+	}, randomDelay);
+
+}
